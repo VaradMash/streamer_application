a9290e3304d9d37ef87f7f5ac30310d3a35f05bf
VaradMash <varadmash2201@gmail.com>
2024-11-23 12:35:08 +0530

    Enhancement in build script and changed logic for displaying blank frame for unopened camera feeds.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-# Build application
 g++ ./src/app.cpp -o ./build/app.out `pkg-config --cflags --libs opencv4`
 
-# Run source
-./build/app.out
+if [[ "$1" == "--run" ]]; then
+    # Run source
+    ./build/app.out
+fi
--- src/video_capture/video_capture_thread.cpp
+++ src/video_capture/video_capture_thread.cpp
@@ -12,11 +12,14 @@ private:
     Mat frame;
     mutex mtx;  // Mutex to protect frame access
     bool isRunning = true;
+    bool isOpened = true;
+    Mat black_frame;
 
 public:
     explicit FeedCapture(Config config) : config(config) {}
 
     void initialize() {
+
         string local_camera = config.get("local_camera");
         if (local_camera == "true") {
             cap.open(0); // Open default local camera
@@ -26,11 +29,14 @@ public:
         }
 
         if (!cap.isOpened()) {
+            this->isOpened = false;
+            this->initializeStaticFrame();
             cerr << "Error: Could not open " << config.get("name") << " feed.\n";
         }
     }
 
     void capture() {
+        if(!cap.isOpened()) return;        
         while (isRunning) {
             Mat tempFrame;
             if (cap.isOpened()) {
@@ -44,6 +50,7 @@ public:
     }
 
     Mat getFrame() {
+        if(!this->isOpened) {return this->black_frame;}
         lock_guard<mutex> lock(mtx); // Lock while accessing the frame
         if (!frame.empty()) {
             Mat annotatedFrame = frame.clone();
@@ -63,5 +70,30 @@ public:
         if (cap.isOpened()) {
             cap.release();
         }
+    }
+
+    Mat initializeStaticFrame() {
+        // Define the dimensions of the black rectangle
+        const int width = 640;
+        const int height = 480;
+        black_frame = Mat::zeros(height, width, CV_8UC3);
+        cout<<"Initializing static frame for "<<config.get("name")<<endl;
+
+        // Define the font and scale
+        int font = FONT_HERSHEY_SIMPLEX;
+        double font_scale = 1.5;
+        int thickness = 2;
+
+        // Get the size of the text box
+        int baseline = 0;
+        Size text_size = getTextSize("No Video", font, font_scale, thickness, &baseline);
+
+        // Calculate the text position to center it in the rectangle
+        Point text_origin((width - text_size.width) / 2, (height + text_size.height) / 2);
+
+        // Add the text to the image
+        putText(black_frame, config.get("name"), Point(10, 30), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 2);
+        putText(black_frame, "No Video", text_origin, font, font_scale, Scalar(255, 255, 255), thickness);
+        return black_frame;
     }
 };
